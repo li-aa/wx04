b2524568af2656f9bde3a0c78bd103d62b5ca3e8
aa <2913404004@qq.com>
2020-11-05 20:03:58 +0800

    配置

--- app/Http/Controllers/TextController.php
+++ app/Http/Controllers/TextController.php
@@ -8,11 +8,29 @@ use Illuminate\Support\Facades\Redis;
 class TextController extends Controller
 {
     public function aa(){
-    	// $c = DB::table('p_users')->limit(3)->get();
-    	// dd($c);
-    	$k = '04';
-    	Redis::set($k,time());
-    	$cc = Redis::get($k);
-    	dd($cc);
+    	$echostr = request()->get("echostr", "");
+        if ($this->checkSignature() && !empty($echostr)) {
+
+            //第一次接入
+            echo $echostr;
+        }
     }
+    private function checkSignature()
+	{
+    $signature = $_GET["signature"];
+    $timestamp = $_GET["timestamp"];
+    $nonce = $_GET["nonce"];
+	
+    $token = 'TOKEN';
+    $tmpArr = array($token, $timestamp, $nonce);
+    sort($tmpArr, SORT_STRING);
+    $tmpStr = implode( $tmpArr );
+    $tmpStr = sha1( $tmpStr );
+    
+    if( $tmpStr == $signature ){
+        return true;
+    }else{
+        return false;
+    }
+	}
 }
