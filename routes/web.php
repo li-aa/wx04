58d8b120c95fa23620111d9b05aafb5f0df12aec
aa <2913404004@qq.com>
2020-11-06 16:37:38 +0800

    a

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 // Route::get('/aa','TextController@aa');
-Route::get('/aa','TextController@wxEvent');
-Route::get('/aa/token','TextController@token');
+Route::any('/wx','TextController@wxEvent');
+Route::get('/wx/token','TextController@token');
