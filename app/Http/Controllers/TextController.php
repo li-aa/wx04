1f2615fe1c1106897fef4573cae1664eb5ad9a43
aa <2913404004@qq.com>
2020-11-11 13:34:04 +0800

    签

--- app/Http/Controllers/TextController.php
+++ app/Http/Controllers/TextController.php
@@ -75,8 +75,8 @@ class TextController extends Controller
                 if (strtolower($data->Event == 'unsubscribe')) {
                     //清除用户的信息
                 }
-                if($postObj->Event == 'click'){
-                    if($postObj->EventKey == 'qian'){
+                if($data->Event == 'click'){
+                    if($data->EventKey == 'qian'){
                         $key = 'qian_'.date('Y_m_d',time());// 例子 USER_SIGN_2020_11_11
 //                        echo $key;die;
                         $content = '签到成功';
